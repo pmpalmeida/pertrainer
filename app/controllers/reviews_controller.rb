5f818cb93b2af9c87e3154b024892b39638bf81a
pedro almeida <pedro.marc2003@gmail.com>
2017-11-11 18:27:21 -0200

    models + controllers sem model city

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+
+  def index         # GET /reviews
+  end
+
+  def show          # GET /reviews/:id
+  end
+
+  def new           # GET /reviews/new
+  end
+
+  def create        # POST /reviews
+  end
+
+  def edit          # GET /reviews/:id/edit
+  end
+
+  def update        # PATCH /reviews/:id
+  end
+
+  def destroy       # DELETE /reviews/:id
+  end
+
+end
---
+++ app/controllers/trainers_controller.rb
@@ -0,0 +1,24 @@
+class TrainersController < ApplicationController
+
+  def index         # GET /trainers
+  end
+
+  def show          # GET /trainers/:id
+  end
+
+  def new           # GET /trainers/new
+  end
+
+  def create        # POST /trainers
+  end
+
+  def edit          # GET /trainers/:id/edit
+  end
+
+  def update        # PATCH /trainers/:id
+  end
+
+  def destroy       # DELETE /trainers/:id
+  end
+
+end
